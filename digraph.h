7edf38f92dba30bc71b1635f8382123799e1a7a7
oxaoo <yol0317@gmail.com>
2016-04-10 22:00:42 +0300

    implemented digraph, declared digraph's methods

---
+++ digraph.h
@@ -0,0 +1,37 @@
+//
+// Created by Alexander on 10.04.2016.
+//
+
+//#ifndef DE_NOVO_DNA_BUILDER_DIGRAPH_H
+//#define DE_NOVO_DNA_BUILDER_DIGRAPH_H
+//
+//#endif //DE_NOVO_DNA_BUILDER_DIGRAPH_H
+
+
+#include <map>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+template<typename T> using adjacency_list_type = map<T, vector<T>>;
+typedef int vertex_type;
+typedef adjacency_list_type<vertex_type> adjacency_list;
+
+class digraph
+{
+private:
+    adjacency_list _digraph;
+
+public:
+    digraph();
+    ~digraph();
+
+    void add_link(vertex_type v1, vertex_type v2);
+    void add_links(istream& stm);
+
+    bool is_linked(vertex_type v1, vertex_type v2);
+    vector<vertex_type> adjacency(vertex_type v);
+    bool is_reach(vertex_type start, vertex_type finish);
+};
