d9c37bb16db3874614f6b60ea22cb6389b28e1e1
Anton Davydov <dydus0x14@gmail.com>
2016-05-16 03:57:18 +0300

    Added docs.

--- DeNovo/Source/mainwindow.cpp
+++ DeNovo/Source/mainwindow.cpp
@@ -193,7 +193,7 @@ void MainWindow::loadFromKmers() {
 
 void MainWindow::checkEuler() {
     Digraph<string>* graph = dynamic_cast<Digraph<string>*>(ui->graph->getGraph());
-    if (EulerianCercuit::isEulerian(graph)) {
+    if (EulerianCercuit::isd(graph)) {
         vector<string> euCycle = EulerianCercuit::getEulerianCircuitVerticies(graph);
         QString result = QString();
         for (typename vector<string>::iterator it = euCycle.begin(); it != euCycle.end(); ++it) {
